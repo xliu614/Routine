5f8a0b8504f2c9821e1af0c2932706fbae8beae9
Vivianne Liu <vivianne@VIVIANNE-HP845G>
2023-11-13 22:35:43 -0800

    Add project files.

---
+++ Routine.Api/Entities/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace Routine.Api.Entities
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Introduction { get; set; }
+
+
+    }
+}
