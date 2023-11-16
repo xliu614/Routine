d50d7af9e5e254c4932218d87fb654c704e728c0
xliu614 <vivianne.liu@gmail.com>
2023-11-15 22:53:50 -0800

    Add DbContext and interface for company repo

--- Routine.Api/Data/RoutineDbContext.cs
+++ Routine.Api/Data/RoutineDbContext.cs
@@ -1,9 +1,54 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Routine.Api.Entities;
 
 namespace Routine.Api.Data
 {
     public class RoutineDbContext:DbContext
     {
+        public RoutineDbContext(DbContextOptions<RoutineDbContext> options): base(options)
+        {
+            
+        }
 
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Company>().Property(x => x.Name).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Company>().Property(x => x.Introduction).HasMaxLength(500);
+
+            modelBuilder.Entity<Employee>().Property(x => x.EmployeeNo).IsRequired().HasMaxLength(10);
+            modelBuilder.Entity<Employee>().Property(x => x.FirstName).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<Employee>().Property(x => x.LastName).IsRequired().HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .HasOne(navigationExpression: x=>x.Company)
+                .WithMany(navigationExpression: x => x.Employees)
+                .HasForeignKey(x => x.CompanyId).OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Company>().HasData(
+                new Company
+                {
+                    Id = Guid.Parse("5836d897-741b-4895-ae31-e6f3f1be1ffd"),
+                    Name = "Google",
+                    Introduction = "Never get in!"
+                },
+                new Company
+                {
+                    Id = Guid.Parse("552bb206-bab4-4c7f-8353-b058a6dc62f4"),
+                    Name = "Microsoft",
+                    Introduction = "Evil source"
+                },
+                new Company
+                {
+                    Id = Guid.Parse("70608d93-5746-4b0b-b73d-88871033a660"),
+                    Name = "Alipapa",
+                    Introduction = "Fubao China"
+                }
+
+           ); 
+        }
     }
 }
---
+++ Routine.Api/Services/ICompanyRepository.cs
@@ -0,0 +1,79 @@
+﻿using Routine.Api.Entities;
+
+namespace Routine.Api.Services
+{
+    public interface ICompanyRepository
+    {
+        /// <summary>
+        /// Get the list of the companies
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<Company>> GetCompaniesAsync();
+        /// <summary>
+        /// Get the list of the companies by the list of Guid
+        /// </summary>
+        /// <param name="companyIds"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Company>> GetCompaniesAsync(IEnumerable<Guid> companyIds);
+        /// <summary>
+        /// Get a company by its Guid
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        Task<Company> GetCompanyAsync(Guid companyId);
+        /// <summary>
+        /// Add a company
+        /// </summary>
+        /// <param name="company"></param>
+        void AddCompany(Company company);
+        /// <summary>
+        /// Update a company
+        /// </summary>
+        /// <param name="company"></param>
+        void UpdateCompany(Company company);
+        /// <summary>
+        /// Remove a company
+        /// </summary>
+        /// <param name="company"></param>
+        void DeleteCompany(Company company);
+        /// <summary>
+        /// Get if the company is already in the system
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        Task<bool> CompanyExistsAsync(Guid companyId);
+
+        /// <summary>
+        /// Get all the employees in a company
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <returns></returns>
+        Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId);
+        /// <summary>
+        /// Get a specific employee
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <param name="employeeId"></param>
+        /// <returns></returns>
+        Task<Employee> GetEmployeeAsync(Guid companyId, Guid employeeId);
+        /// <summary>
+        /// Add employee
+        /// </summary>
+        /// <param name="companyId"></param>
+        /// <param name="employee"></param>
+        void AddEmployee(Guid companyId, Employee employee);
+        /// <summary>
+        /// Update employee
+        /// </summary>
+        /// <param name="employee"></param>
+        void UpdateEmployee(Employee employee);
+        /// <summary>
+        /// Remove employee
+        /// </summary>
+        /// <param name="employee"></param>
+        void DeleteEmployee(Employee employee);
+
+        Task<bool> SaveAsync();
+   
+    }
+}
