37231cc9f0bab0c4d9d2adc834ccf727cc6136fe
xliu614 <vivianne.liu@gmail.com>
2023-12-18 14:49:31 -0800

    PropertyMappingService should implement IPropertyMappingService

--- Routine.Api/Services/PropertyMappingService.cs
+++ Routine.Api/Services/PropertyMappingService.cs
@@ -1,6 +1,34 @@
-﻿namespace Routine.Api.Services
+﻿using Routine.Api.Entities;
+using Routine.Api.Models;
+
+namespace Routine.Api.Services
 {
-    public class PropertyMappingService
+    public class PropertyMappingService:IPropertyMappingService
     {
+        private Dictionary<string, PropertyMappingValue> _employeePropertyMapping = new Dictionary<string, PropertyMappingValue>(StringComparer.OrdinalIgnoreCase) {
+            {"Id", new PropertyMappingValue(new List<string> {"Id"}) },
+            {"CompanyId", new PropertyMappingValue(new List<string> {"CompanyId"}) },
+            {"EmployeeNo", new PropertyMappingValue(new List<string> {"EmployeeNo" }) },
+            {"Name", new PropertyMappingValue(new List<string> {"FirstName", "LastName" }) },
+            {"GenderDisplay", new PropertyMappingValue(new List<string> {"Gender"}) },
+            {"Age", new PropertyMappingValue(new List<string> {"DateOfBirth" }, revert: true) }
+        };
+
+        private IList<IPropertyMapping> _propertyMapping = new List<IPropertyMapping>();
+
+        public PropertyMappingService()
+        {
+            _propertyMapping.Add(new PropertyMapping<EmployeeDto, Employee>(_employeePropertyMapping));
+        }
+        public Dictionary<string, PropertyMappingValue> GetPropertyMapping<TSource, TDestination>() {
+            var matchingMapping = _propertyMapping.OfType<PropertyMapping<TSource, TDestination>>();
+            var propertyMappings = matchingMapping.ToList();
+
+            if (propertyMappings.Count() == 1)
+                return matchingMapping.First().MappingDictionary;
+
+            throw new Exception($"Cannot find unique mapping: {typeof(TSource)}, {typeof(TDestination)} ");
+        }
+
     }
 }
